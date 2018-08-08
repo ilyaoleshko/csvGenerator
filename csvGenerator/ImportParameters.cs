933c3505a767d43a3e38fa799fe1a46408d40aa3
Ilya Oleshko <oleshko.ilya@gmail.com>
2018-08-08 16:42:17 +0300

    Импорт генератора csv файлов с использованием библиотеки CsvHelper.7.1.1

---
+++ csvGenerator/ImportParameters.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csvGenerator
+{
+    internal class ImportParameters
+    {
+        internal string Title { get; private set; }
+        internal int Id { get; private set; }
+
+        protected ImportParameters(string title, int id)
+        {
+            Title = title;
+            Id = id;
+        }
+    }
+
+    internal class ImportParameters<T> : ImportParameters
+    {
+        internal T Value { get; private set; }
+
+        protected ImportParameters(string title, T value, int id) : base(title, id)
+        {
+            Value = value;
+        }
+    }
+
+    internal class SeparatorParameters : ImportParameters<string>
+    {
+        internal static List<SeparatorParameters> Separator = new List<SeparatorParameters>
+        {
+            new SeparatorParameters("Comma", ",", 0),
+            new SeparatorParameters("Semicolon", ";", 1),
+            new SeparatorParameters("Colon", ":", 2),
+            new SeparatorParameters("Tab", "\t", 3),
+            new SeparatorParameters("Space", " ", 4)
+        };
+
+        private SeparatorParameters(string title, string value, int id)
+            : base(title, value, id)
+        {
+        }
+    }
+
+    internal class EncodingParameters : ImportParameters<Encoding>
+    {
+        internal static List<EncodingParameters> Encodng = new List<EncodingParameters>
+        {
+            new EncodingParameters("UTF8", Encoding.UTF8, 0),
+            new EncodingParameters("ASCII", Encoding.ASCII, 1),
+            new EncodingParameters("Windows1251", Encoding.GetEncoding(1251), 2),
+            new EncodingParameters("CP866", Encoding.GetEncoding(866), 3),
+            new EncodingParameters("KOI8R", Encoding.GetEncoding(20866), 4)
+        };
+
+        private EncodingParameters(string title, Encoding value, int id)
+            : base(title, value, id)
+        {
+        }
+    }
+
+    internal class DelimiterParameters : ImportParameters<string>
+    {
+        internal static List<DelimiterParameters> Delimiter = new List<DelimiterParameters>
+        {
+            new DelimiterParameters("DQ", "\"", 0),
+            new DelimiterParameters("SQ", "\'", 1)
+        };
+
+        private DelimiterParameters(string title, string value, int id) 
+            : base(title, value, id)
+        {
+        }
+    }
+}
---
+++ csvGenerator/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.IO;
+using System.Text;
+using CsvHelper;
+
+namespace csvGenerator
+{
+    internal class Program
+    {
+        public static void Generate(SeparatorParameters sep, EncodingParameters enc, int count)
+        {
+            using (TextWriter writer =
+                new StreamWriter(@"TestCsv\" + enc.Title + "_" + sep.Title + "_NQ.csv", false, enc.Value))
+            {
+                var csv = new CsvWriter(writer);
+                csv.Configuration.QuoteNoFields = true;
+                for (var i = 0; i < count; i++)
+                {
+                    csv.WriteField(i + "default.post@gmail.com" + sep.Value + i + "Ilya" + sep.Value + i + "Oleshko");
+                    csv.NextRecord();
+                }
+            }
+        }
+
+        public static void GenerateChangePosition(int count)
+        {
+            using (TextWriter writer =
+                new StreamWriter(@"TestCsv\Default_parameters_Wich_header_Change_position.csv", false, Encoding.UTF8))
+            {
+                var csv = new CsvWriter(writer);
+                csv.Configuration.QuoteNoFields = true;
+                csv.WriteField("LastName,Email,FirstName");
+                csv.NextRecord();
+                for (var i = 0; i < count; i++)
+                {
+                    csv.WriteField(i + "Oleshko,default.post@gmail.com,Ilya");
+                    csv.NextRecord();
+                }
+            }
+        }
+
+        public static void GenerateDefaultPosition(int count)
+        {
+            using (TextWriter writer =
+                new StreamWriter(@"TestCsv\Default_parameters_Wich_header_Default_position.csv", false, Encoding.UTF8))
+            {
+                var csv = new CsvWriter(writer);
+                csv.Configuration.QuoteNoFields = true;
+                csv.WriteField("FirstName,LastName,Email");
+                csv.NextRecord();
+                for (var i = 0; i < count; i++)
+                {
+                    csv.WriteField(i + "Ilya,Oleshko,default.post@gmail.com");
+                    csv.NextRecord();
+                }
+            }
+        }
+
+        public static void GenerateWithDelimiter(SeparatorParameters sep, EncodingParameters enc, DelimiterParameters del, int count)
+        {
+            using (TextWriter writer =
+                new StreamWriter(@"TestCsv\" + enc.Title + "_" + sep.Title + "_" + del.Title + ".csv", false, Encoding.UTF8))
+            {
+                var csv = new CsvWriter(writer);
+                csv.Configuration.QuoteNoFields = true;
+                for (var i = 0; i < count; i++)
+                {
+                    csv.WriteField(i + "default.post@gmail.com" + sep.Value + del.Value + i + "Ilya" + del.Value + sep.Value + i + "Oleshko");
+                    csv.NextRecord();
+                }
+            }
+        }
+
+        private static void Main()
+        {
+            const int count = 5;
+            GenerateDefaultPosition(count);
+            GenerateChangePosition(count);
+            var i = SeparatorParameters.Separator.Count;
+            while (i > 0)
+            {
+                i--;
+                Generate(SeparatorParameters.Separator[i], EncodingParameters.Encodng[0], count);
+            }
+
+            var j = EncodingParameters.Encodng.Count;
+            while (j > 0)
+            {
+                j--;
+                Generate(SeparatorParameters.Separator[0], EncodingParameters.Encodng[j], count);
+            }
+
+            var k = DelimiterParameters.Delimiter.Count;
+            while (k > 0)
+            {
+                k--;
+                GenerateWithDelimiter(SeparatorParameters.Separator[0], EncodingParameters.Encodng[0], DelimiterParameters.Delimiter[k], count);
+            }
+        }
+    }
+}
